c792f97e774bf1f91f7235ab441d17db118f331f
Md.  Raihan Hossain Jibon <53583320+rjibon49@users.noreply.github.com>
2022-03-11 21:41:37 +0600

    update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { MongoClient, ServerApiVersion  } = require('mongodb');
 const cors = require ('cors')
 const ObjectId = require("mongodb").ObjectId;
 
+require("dotenv").config();
+
 
 const app = express();
 
@@ -13,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://rjibon49:WdrMnCSslW9WjoZe@cluster0.ggbuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ggbuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
